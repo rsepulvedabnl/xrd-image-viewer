7be50c2af3ac16c6bc9430cb284eaf18f1d3c958
Ramon Sepulveda <rsepulved@bnl.gov>
2021-08-01 10:28:02 -0400

    Removed leftover directories.

--- xrd_image_viewer/plotting/__init__.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.cm as cm
-from ..hdfdata import HdfData
-
-class DataPlotter:
-    @staticmethod
-    def plotpath(path, ax, min, max):
-        arr = HdfData.getarrayfrompath(path)
-        ax.pcolormesh(arr, cmap = cm.gist_yarg, vmin = min, vmax = max)
